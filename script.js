901e733e3fed2cfa3c48ffdaaeae92bb41e896f3
SonnyV2 <sonnyliardpro@gmail.com>
2025-03-21 12:05:49 +0100

    21/03 12h05

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {ProgressBar()};
+
+function ProgressBar() {
+  const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  const scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
+
+
